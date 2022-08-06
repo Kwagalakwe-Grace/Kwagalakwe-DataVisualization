3579deca520b0c59b9f9a3c361a2c1427f8eab9b
Kwagalakwe-Grace <105932768+Kwagalakwe-Grace@users.noreply.github.com>
2022-08-06 08:46:55 +0300

    Update bch.js

--- bch.js
+++ bch.js
@@ -1,10 +1,11 @@
 var svg = d3.select("#barchart")
 			.append("svg")
+      //.orient(left)
 			.attr("height", 200)
       .attr("width", 1000);
 
 
-
+// Logic to handle Tooltip on Hover of Bar
 var hoveron = function(d) {
       console.log('d', d, 'event', event);
       var div = document.getElementById('tooltip');
@@ -12,28 +13,30 @@ var hoveron = function(d) {
       div.style.top = event.pageY + 'px';
 
       
-     
+      //Fill white to highlight
       d3.select(this)
         .style("fill", "white");
 
-      
+      //Show the tooltip
       d3.select("#tooltip")
         .style("opacity", 1);
 
-
+      //Populate name in tooltip
       d3.select("#tooltip .name")
         .text(d.District);
+
+      //Populate value in tooltip
       d3.select("#tooltip .value")
         .text(d.Percentage +"%"); 
 }
 
 var hoverout = function(d) {
 
-
+  //Restore original color fill
   d3.select(this)
     .style("fill", "");
 
- 
+  //Hide the tooltip
   d3.select("#tooltip")
     .style("opacity", 0);
 
@@ -49,7 +52,9 @@ d3.csv("barchart.csv", function(data) {
 					.enter()
 					.append("rect");
 
-	rects.attr("y",0)
+	rects.attr("y", function(d) {
+      return 200 - d.Percentage; 
+    })
 		.attr("x", function(d, i) {
 			return i*6.8;
 		})
